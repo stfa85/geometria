93d23c32aa1b9da2bca4e6f5f24be134d4c53a4e
stfa <stfa85@gmail.com>
2022-02-27 21:52:52 +0100

    version comentada

--- geometria/src/Rectangulo_EAQ.java
+++ geometria/src/Rectangulo_EAQ.java
@@ -1,20 +1,41 @@
-
+/**
+ * 
+ * @author Stfa
+ * @nombre Estefania
+ * @apellidos Alfaro Quiles
+ * @version 1.2
+ * 
+ */
 public class Rectangulo extends FiguraGeometrica {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * 
+	 * @param tipoFigura recoge el tipo de la figura geometrica
+	 * @param lG medida de lado 1
+	 * @param lP medida de lado 2
+	 */
 	public Rectangulo(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+	
 	@Override
+	/**
+	 * 
+	 * @return el area de la figura
+	 */
 	public double area() {
 		return l1 * l2;
 	}
 	
 	@Override
+	/**
+	 * 
+	 * @return el perimetro de la figura
+	 */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
