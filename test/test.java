5a5ced6abc97d72f853c873c71127fd2a5d8b48c
stfa <stfa85@gmail.com>
2022-02-27 22:17:34 +0100

    Uff ahora si

---
+++ test/test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Stfa
+ *
+ */
+public class test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		radio = 7;
+	}
+
+}
